7f4356adc36a2db16a6cf0df5afafc3867839c32
aslampangestu <aslampangestu@gmail.com>
2019-08-19 13:13:31 +0700

    init babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-(function() {
-  console.log("HALLO");
-});
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "index.scss";
+
+let HelloWorld = () => {
+  return <h1>Hello World</h1>;
+};
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
